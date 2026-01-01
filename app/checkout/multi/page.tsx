d214855d64984bb1513c064caf8f21a261fdf549
Focus <Focus@Brad1s-MacBook-Pro.local>
2026-01-01 22:54:28 +0200

    Add 'use client' directive to checkout multi page

--- app/checkout/multi/page.tsx
+++ app/checkout/multi/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+'use client';
+
 import { useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { CreditCard, Building2, Lock, Loader2, AlertCircle, Package } from 'lucide-react';
